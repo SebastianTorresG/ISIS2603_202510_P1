9d14dc1ff959a47a6a39a4b2334eec55d46a979e
SebastianTorresG <123436366+SebastianTorresG@users.noreply.github.com>
2025-03-07 07:57:03 -0500

    Add unit tests for ConciertoService and EstadioService with initial data setup

---
+++ src/test/java/co/edu/uniandes/dse/parcial1/services/EstadioServiceTest.java
@@ -0,0 +1,55 @@
+package co.edu.uniandes.dse.parcial1.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import co.edu.uniandes.dse.parcial1.entities.ConciertoEntity;
+import co.edu.uniandes.dse.parcial1.entities.EstadioEntity;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+public class EstadioServiceTest {
+    @Autowired
+    private EstadioService authorService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private PodamFactory factory = new PodamFactoryImpl();
+
+    private List<EstadioEntity> authorList = new ArrayList<>();
+
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @BeforeEach
+    void setUp() {
+        clearData();
+        insertData();
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        entityManager.getEntityManager().createQuery("delete from PrizeEntity").executeUpdate();
+        entityManager.getEntityManager().createQuery("delete from BookEntity").executeUpdate();
+        entityManager.getEntityManager().createQuery("delete from AuthorEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            EstadioEntity authorEntity = factory.manufacturePojo(EstadioEntity.class);
+            entityManager.persist(authorEntity);
+            authorList.add(authorEntity);
+        }
+    }
+
+}
